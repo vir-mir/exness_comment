25ee765468928dba53e4ecb300b5b591c77aaba4
virmir49 <virmir49@gmail.com>
2016-09-08 20:54:52 +0300

    config env

--- exness_comment/configs/settings.py
+++ exness_comment/configs/settings.py
@@ -10,8 +10,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.abspath(os.path.join(PROJECT_ROOT, '..', 'media'))
 
 DATA_BASE = {
-    'username': 'vir-mir',
-    'password': 'rf,fkbcnbrf',
-    'host': 'localhost',
-    'db': 'exness_comment',
+    'username': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PWD'),
+    'host': os.getenv('DB_HOST'),
+    'db': os.getenv('DB_NAME'),
 }
