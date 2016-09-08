75652e33880bef0f7fe1a20c01c0ec2ad963f170
virmir49 <virmir49@gmail.com>
2016-09-08 22:00:15 +0300

    fix pagination

--- exness_comment/views/commet/commets.py
+++ exness_comment/views/commet/commets.py
@@ -42,8 +42,8 @@ class Comments(MixinComment):
 
     async def get_first_level_comments(self, data):
         limit = data['limit']
-        offset = 0 if data['page'] == 1 else data['page'] * data['limit']
-
+        offset = (data['page'] - 1) * data['limit']
+        print(offset)
         query = (sa.select(self.fields)
                  .select_from(Comment)
                  .where(sa.and_(Comment.level == 0, Comment.entity_id == data['entity_id']))
